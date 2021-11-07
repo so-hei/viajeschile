e6f8fa3ed2fa49c623fb6bebfa659f49899da29b
Sofía <sofi.heisig@gmail.com>
2021-11-07 05:10:56 -0300

    agregando js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#enviar").click(function () {
+    alert("El correo fue enviado, te responderemos en dentro de 24 horas")
+  });
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
