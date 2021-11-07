68dda552acdc1d378e27d3a64dac930f80ece806
Sofía <sofi.heisig@gmail.com>
2021-11-07 06:05:25 -0300

    formulario y funcion scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 $(function () {
+    $(window).scroll(function () {
+      var navegador = $(".navbar");
+      if ($(this).scrollTop() > 300) {
+        navegador.css("background", "#3AA1B0");
+      } else {
+        navegador.css("background", "none");
+      }
+    });
+});
+
+$(function () {
   $("#enviar").click(function () {
     alert("El correo fue enviado, te responderemos en dentro de 24 horas")
   });
